bb669da7056a2ccde3032ea1713f6a57450edc3b
Raphael Hernandez <raphhernandez1220@gmail.com>
2022-05-12 11:59:21 +0200

    Creation pages et navbar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+   <div class="about">
+      <h1>This is an Home page</h1>
+  </div>
   </main>
 </template>
